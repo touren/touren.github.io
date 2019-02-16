0b3575b8a37ccb8ef0e6a13d7905eea1e332f536
rentao <tour_ren@163.com>
2019-02-16 07:49:09 +0000

    Fixed bugs in ubuntu18.

--- serve.sh
+++ serve.sh
@@ -1,3 +1,9 @@
 echo More detail:  https://help.github.com/articles/setting-up-your-github-pages-site-locally-with-jekyll/
+
+# To fixed issue "Invalid US-ASCII character "\xE2"", See: https://github.com/jekyll/jekyll/issues/4268
+export LANGUAGE="en_US.UTF-8"
+export LC_ALL="C.UTF-8"
+export LANG="en_US.UTF-8"
+
 bundle exec jekyll serve
 
